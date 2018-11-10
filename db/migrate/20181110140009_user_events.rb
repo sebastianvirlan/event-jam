321a7d95e8495d89993a79459fde0c9bc5720947
Sebastian Vîrlan <sebastian.virlan@yahoo.com>
2018-11-10 14:04:49 +0000

    Add event model, and migrations for events and user_events

---
+++ db/migrate/20181110140009_user_events.rb
@@ -0,0 +1,9 @@
+class UserEvents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :user_events, id: false do |t|
+      t.references :user
+      t.references :event
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20181110140358_add_uniqueness_for_user_events.rb
@@ -0,0 +1,5 @@
+class AddUniquenessForUserEvents < ActiveRecord::Migration[5.2]
+  def change
+    add_index :users, [:user_id, :event_id], unique: true
+  end
+end
