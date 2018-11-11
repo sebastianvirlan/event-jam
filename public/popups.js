53c7cda3299a580167fa2f3ce440e96ddc1bdb6e
Sebastian Vîrlan <sebastian.virlan@yahoo.com>
2018-11-11 07:33:06 +0000

    Ads added, table forms

---
+++ public/popups.js
@@ -0,0 +1,22 @@
+function a1(url) {
+  var strWindowFeatures = "location=yes,height=570,width=520,scrollbars=yes,status=yes";
+
+  window.open(
+    "https://livebetting.bwin.com/en/live#/overview", "_blank", strWindowFeatures + ",top=0,left=0"
+  )
+  window.open(
+    "https://m.skybet.com", '_blank', strWindowFeatures + ",top=0,left=800"
+  )
+  popupWindow2 = window.open(
+    "https://www.unibet.co.uk/betting?crossSiteRedirect=true", '_blank', strWindowFeatures + ",top=0,left=520"
+  )
+  window.open(
+    "https://www.betfair.com/sport/inplay", '_blank', strWindowFeatures + ",top=570,left=0"
+  )
+  window.open(
+    "https://www.paddypower.com/bet", '_blank', strWindowFeatures + ",top=570,left=800"
+  )
+  window.open(
+    "https://www.mrgreen.com/en/betting", '_blank', strWindowFeatures + ",top=890,left=300"
+  )
+}
