a2a8d56c79a837fccb6e14298568a416386c9e4f
Sebastian Vîrlan <sebastian.virlan@yahoo.com>
2018-11-11 02:13:58 +0000

    Addded registration and login

--- db/migrate/20181110135527_add_fields_to_users.rb
+++ db/migrate/20181110135527_add_fields_to_users.rb
@@ -1,8 +1,6 @@
 class AddFieldsToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :username, :string
-    add_column :users, :first_name, :string
-    add_column :users, :last_name, :string
-    add_index :users, :username, unique: true
+    add_column :users, :first_name, :string, after: :email
+    add_column :users, :last_name, :string, after: :email
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -45,12 +45,10 @@ ActiveRecord::Schema.define(version: 2018_11_10_185007) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username"
     t.string "first_name"
     t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
     t.index [nil, nil], name: "index_users_on_user_id_and_event_id", unique: true
   end
 
